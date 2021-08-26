13f069be65a7575ac78529c4542202affc93221d
Leonid <bychkov.l47@mail.ru>
2021-08-27 00:35:13 +0300

    started slider

--- slider/slider.js
+++ slider/slider.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', e => {
-
+    const swiper = new Swiper('.swiper', {
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        spaceBetween: 20,
+        grabCursor: true,
+    });
 });
